c23046832372c9a283ecc3dff3902c8b4a0d8384
yurIFSP <y.menon@aluno.ifsp.edu.br>
2023-09-26 13:29:33 -0300

    feat: Add second test to check for integer inputs

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -15,4 +15,12 @@ class MainTest {
         assertEquals("Erro",Main.calcAcimaDaMedia(new String[]{"10", "11", "12", "13", "14", "15", "a"}));
         assertEquals("0",Main.calcAcimaDaMedia(new String[]{"10", "10", "10", "10", "10", "10", "10"}));
     }
+
+    @Test
+    void checarNumerosReais(){
+        assertEquals("Erro",Main.calcAcimaDaMedia(new String[]{"1.0"}));
+        assertEquals("Erro",Main.calcAcimaDaMedia(new String[]{"1.0", "11", "12", "13", "14", "15", "16"}));
+        assertEquals("Erro",Main.calcAcimaDaMedia(new String[]{"10", "11", "12", "13", "14", "15", "1.0"}));
+        assertEquals("0",Main.calcAcimaDaMedia(new String[]{"10", "10", "10", "10", "10", "10", "10"}));
+    }
 }
